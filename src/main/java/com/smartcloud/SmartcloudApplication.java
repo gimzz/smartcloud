5fa992b847d38b95e215e770ad5acbec9f5b6088
Gimzz <letig732@gmail.com>
2026-02-07 21:37:20 -0400

    chore: initialize Spring Boot project

---
+++ src/main/java/com/smartcloud/SmartcloudApplication.java
@@ -0,0 +1,13 @@
+package com.smartcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcloud/SmartcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
